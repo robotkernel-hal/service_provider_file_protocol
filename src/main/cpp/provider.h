01e25048262ef6866f72f4b1bb244febe96e886b
Robert Burger <robert.burger@dlr.de>
2018-02-16 10:20:34 +0100

    auto commit

--- src/main/cpp/provider.h
+++ src/main/cpp/provider.h
@@ -34,56 +34,69 @@
 #include "service_provider/file_protocol/base.h"
 
 namespace service_provider {
+#ifdef EMACS
+}
+#endif
 
-    namespace file_protocol {
+namespace file_protocol {
+#ifdef EMACS
+}
+#endif
 
-        // forward declaration
-        class handler;
+// forward declaration
+class handler;
 
-        class provider : public robotkernel::service_provider_base<handler, base> {
-            public:
-                //! default construction
-                /*!
-                 * \param node configuration node
-                 */
-                provider()
-                    : service_provider_base("file_protocol") {};
-        };
+class provider : public robotkernel::service_provider_base<handler, base> {
+    public:
+        //! default construction
+        /*!
+         * \param node configuration node
+         */
+        provider(const std::string& name)
+            : service_provider_base(name, "file_protocol") {};
+};
 
-        class handler : public robotkernel::log_base {
-            public:
-                typedef std::shared_ptr<service_provider::file_protocol::base> sp_cp_base_t;
-                sp_cp_base_t _instance;
+class handler : public robotkernel::log_base {
+    public:
+        typedef std::shared_ptr<service_provider::file_protocol::base> sp_cp_base_t;
+        sp_cp_base_t _instance;
 
-                //! handler construction
-                handler(const robotkernel::sp_service_interface_t& req);
+        //! handler construction
+        handler(const robotkernel::sp_service_interface_t& req);
 
-                //! handler destruction
-                ~handler();
+        //! handler destruction
+        ~handler();
 
-                //! service callback request file read
-                /*!
-                 * \param request service request data
-                 * \parma response service response data
-                 * \return success
-                 */
-                int service_file_read(const robotkernel::service_arglist_t& request, 
-                        robotkernel::service_arglist_t& response);
-                static const std::string service_definition_file_read;  
+        //! service callback request file read
+        /*!
+         * \param request service request data
+         * \parma response service response data
+         * \return success
+         */
+        int service_file_read(const robotkernel::service_arglist_t& request, 
+                robotkernel::service_arglist_t& response);
+        static const std::string service_definition_file_read;  
 
-                //! service callback request file write
-                /*!
-                 * \param request service request data
-                 * \parma response service response data
-                 * \return success
-                 */
-                int service_file_write(const robotkernel::service_arglist_t& request, 
-                        robotkernel::service_arglist_t& response);
-                static const std::string service_definition_file_write; 
-        };
+        //! service callback request file write
+        /*!
+         * \param request service request data
+         * \parma response service response data
+         * \return success
+         */
+        int service_file_write(const robotkernel::service_arglist_t& request, 
+                robotkernel::service_arglist_t& response);
+        static const std::string service_definition_file_write; 
+};
 
-    }; // namespace file_protocol
+#ifdef EMACS
+{
+#endif
+}; // namespace file_protocol
 
+
+#ifdef EMACS
+{
+#endif
 }; // namespace service_provider
 
 #endif // __SERVICE_PROVIDER__FILE_PROTOCOL__PROVIDER_H__
