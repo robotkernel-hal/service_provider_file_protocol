1b1184c0fff691204cc2a7da5e600ab5ffade87f
Robert Burger <robert.burger@dlr.de>
2023-02-20 12:24:55 +0100

    change: releasing python_rkgui as separate package

--- conanfile.py
+++ conanfile.py
@@ -14,10 +14,3 @@ class MainProject(ConanFile):
         self.requires(f"{self.name}_ln_msgdef/{self.version}@{self.user}/{self.channel}")
         self.requires("robotkernel_service_helper/[*]@robotkernel/stable")
         self.requires("robotkernel/[~=5]@robotkernel/stable")
-
-    def package_info(self):
-        base = self.python_requires["conan_template"].module.RobotkernelConanFile
-        base.package_info(self)
-
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "bindings/python"))
-
