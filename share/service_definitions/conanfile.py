e4d9a740fa6dd4b4e4e5a255b67acd94630a53df
Robert Burger <robert.burger@dlr.de>
2026-02-12 06:56:19 +0100

    change: reworked for new services

--- share/service_definitions/conanfile.py
+++ share/service_definitions/conanfile.py
@@ -15,7 +15,7 @@ class MainProject(ConanFile):
         "*",
     ]
 
-    tool_requires = ["robotkernel_ln_helper/[>=0.0.7]@robotkernel/stable"]
+    tool_requires = ["robotkernel_ln_helper/[~6]@robotkernel/unstable"]
     generators = "VirtualBuildEnv"
 
     def package(self):
